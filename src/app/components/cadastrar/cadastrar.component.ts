76066ee18248c35527e3b298d19a877c12b36376
Artur Lampert <arturslampert@gmail.com>
2025-01-24 16:36:42 -0300

    Criando a tela de cadastro

--- src/app/components/cadastrar/cadastrar.component.ts
+++ src/app/components/cadastrar/cadastrar.component.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cadastrar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cadastrar.component.html',
   styleUrl: './cadastrar.component.css'
 })
 export class CadastrarComponent {
+  items: string[] = [];
+
+
+
+  addToArray(value: string) {
+
+    if (value) {
+
+      this.items.push(value);
+
+    }
+
+  }
 
 }
