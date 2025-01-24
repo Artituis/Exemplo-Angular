0c01423d012ea122cb514e1c3f255d12b4051265
Artur Lampert <arturslampert@gmail.com>
2025-01-24 16:27:50 -0300

    Criando rota de cadastrar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { CadastrarComponent } from './components/cadastrar/cadastrar.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'cadastrar',
+        component: CadastrarComponent
     }
 ];
